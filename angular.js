2999bb2527a2d02f3c9559df7f06bc94cb82e5fe
Florian Symanowski <iceteabottle@users.noreply.github.com>
2026-06-23 10:06:46 +0200

    Extract rules for Angular into separate files

--- angular.js
+++ angular.js
@@ -1,58 +1,22 @@
-const angularEslint = require('@angular-eslint/eslint-plugin');
-const angularTemplateParser = require('@angular-eslint/template-parser');
-const angularTemplatePlugin = require('@angular-eslint/eslint-plugin-template');
-const changeDetectionStrategy = require('eslint-plugin-change-detection-strategy');
-
 module.exports = [
   // Angular TypeScript files
   {
     files: ['**/*.ts'],
     plugins: {
-      '@angular-eslint': angularEslint,
-      'change-detection-strategy': changeDetectionStrategy,
+      '@angular-eslint': require('@angular-eslint/eslint-plugin'),
+      'change-detection-strategy': require('eslint-plugin-change-detection-strategy'),
     },
-    rules: {
-      '@angular-eslint/component-selector': [
-        'error',
-        {
-          prefix: 'app',
-          style: 'kebab-case',
-          type: 'element',
-        },
-      ],
-      '@angular-eslint/directive-selector': [
-        'error',
-        {
-          prefix: 'app',
-          style: 'camelCase',
-          type: 'attribute',
-        },
-      ],
-      '@angular-eslint/no-conflicting-lifecycle': 'error',
-      '@angular-eslint/no-input-rename': 'error',
-      '@angular-eslint/no-inputs-metadata-property': 'error',
-      '@angular-eslint/no-output-native': 'error',
-      '@angular-eslint/no-output-on-prefix': 'error',
-      '@angular-eslint/no-output-rename': 'error',
-      '@angular-eslint/no-outputs-metadata-property': 'error',
-      '@angular-eslint/use-lifecycle-interface': 'error',
-      '@angular-eslint/use-pipe-transform-interface': 'error',
-      'change-detection-strategy/on-push': 'error',
-    },
+    rules: require('./angular/ts/rules'),
   },
   // Angular template files
   {
     files: ['**/*.component.html'],
     languageOptions: {
-      parser: angularTemplateParser,
+      parser: require('@angular-eslint/template-parser'),
     },
     plugins: {
-      '@angular-eslint/template': angularTemplatePlugin,
+      '@angular-eslint/template': require('@angular-eslint/eslint-plugin-template'),
     },
-    rules: {
-      '@angular-eslint/template/banana-in-box': 'error',
-      '@angular-eslint/template/eqeqeq': 'error',
-      '@angular-eslint/template/no-negated-async': 'error',
-    },
+    rules: require('./angular/html/rules'),
   },
 ];
